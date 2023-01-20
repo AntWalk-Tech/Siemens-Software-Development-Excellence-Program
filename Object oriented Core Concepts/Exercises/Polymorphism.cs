684487652ce889747acdae8a3e1eca7f582ebd44
Athika Fatima <athikafatima16@gmail.com>
2023-01-20 08:15:18 +0530

    Added 2 exercises for Polymorphism with solutions

---
+++ Object oriented Core Concepts/Exercises/Polymorphism.cs
@@ -0,0 +1,16 @@
+// Exercise 1: Create an abstract class called Shape with an abstract method called Draw and a private field _color. 
+// Now create two classes called Rectangle and Circle that inherit from the Shape class. 
+// Override the Draw method in each child class to draw a rectangle and a circle respectively. 
+// In main method create an array of Shape objects, instantiate a rectangle and a circle, and add them to the array. 
+// Then call the Draw method on each object in the array.
+
+
+
+
+
+
+// Exercise 2: Create an interface called ICalculator with a method called Calculate that takes two integers as arguments and returns an integer. 
+// Now create two classes called Addition and Multiplication that implement the ICalculator interface. 
+// Override the Calculate method in each class to perform addition and multiplication respectively. 
+// In main method create an array of ICalculator objects, instantiate an addition and a multiplication object, and add them to the array. 
+// Then call the Calculate method on each object in the array passing the values 2 and 3.
---
+++ Object oriented Core Concepts/Exercises/Solutions/polymorphism-sol.cs
@@ -0,0 +1,68 @@
+// Exercise 1: Create an abstract class called Shape with an abstract method called Draw and a private field _color. 
+// Now create two classes called Rectangle and Circle that inherit from the Shape class. 
+// Override the Draw method in each child class to draw a rectangle and a circle respectively. 
+// In main method create an array of Shape objects, instantiate a rectangle and a circle, and add them to the array. 
+// Then call the Draw method on each object in the array.
+
+// SOLUTION: 
+
+abstract class Shape
+{
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public abstract void Draw();
+}
+class Rectangle : Shape
+{
+    public Rectangle(string color) : base(color)
+    {
+    }
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle in " + _color + " color.");
+    }
+}
+class Circle : Shape
+{
+    public Circle(string color) : base(color)
+    {
+    }
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle in " + _color + " color.");
+    }
+}
+
+
+
+
+
+// Exercise 2: Create an interface called ICalculator with a method called Calculate that takes two integers as arguments and returns an integer. 
+// Now create two classes called Addition and Multiplication that implement the ICalculator interface. 
+// Override the Calculate method in each class to perform addition and multiplication respectively. 
+// In main method create an array of ICalculator objects, instantiate an addition and a multiplication object, and add them to the array. 
+// Then call the Calculate method on each object in the array passing the values 2 and 3.
+
+// SOLUTION:
+
+interface ICalculator
+{
+    int Calculate(int a, int b);
+}
+class Addition : ICalculator
+{
+    public int Calculate(int a, int b)
+    {
+        return a + b;
+    }
+}
+class Multiplication : ICalculator
+{
+    public int Calculate(int a, int b)
+    {
+        return a * b;
+    }
+}
