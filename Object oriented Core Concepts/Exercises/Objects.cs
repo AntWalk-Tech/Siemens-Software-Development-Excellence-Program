58877a0591583f0be5ac164f206b384ba2159b02
Athika Fatima <athikafatima16@gmail.com>
2023-01-20 08:11:39 +0530

    Added 2 exercises for Objects with solutions

---
+++ Object oriented Core Concepts/Exercises/Objects.cs
@@ -0,0 +1,12 @@
+// Exercise 1: Create a class called Person with a private field _name and a public property Name. 
+// Also add a private field _age and a public property Age. Now create a constructor that initializes the fields and a method Greet that returns a greeting message using the person's name. 
+// In main method create a new object of the class and assign a name and age to it then call the greet method.
+
+
+
+
+
+// Exercise 2: Create a class called Book with private fields such as _title, _author, _pages, and _price. 
+// Now add a constructor that initializes these fields and public properties that provide read-only access to the fields. 
+// Also add a method called GetBookInfo that returns a string with the book's title, author, number of pages, and price. 
+// Create an object of the class and call the method to get the book's information.
